f99edc94910c7b16749c37e93173b557ac594aaf
qiwi <qiwi@qiwi.qiwi>
2025-11-01 19:10:40 +0300

    added flatten function

--- main.cpp
+++ main.cpp
@@ -44,6 +44,15 @@ void output(int **mtx, size_t rows, size_t columns) {
     std::cout << "\n";
   }
 }
+int *flatten(int **matrix, size_t rows, size_t columns) {
+  int *result = new int[rows * columns];
+  for (size_t i = 0, cntr = 0; i < rows; i++) {
+    for (size_t j = 0; j < columns; j++, cntr++) {
+      result[cntr] = matrix[i][j];
+    }
+  }
+  return result;
+}
 int main() {
   int **matrix = nullptr;
   size_t r = 0, c = 0;
@@ -55,6 +64,12 @@ int main() {
       return 1;
     }
     output(matrix, r, c);
+    int *list = flatten(matrix, r, c);
+    // вывод flattend
+    for (size_t i = 0; i < c * r; i++) {
+      std::cout << list[i] << " ";
+    }
+    std::cout << "\n";
     destroy(matrix, r * c);
   } catch (const std::bad_alloc &e) {
     std::cout << e.what() << std::endl;
