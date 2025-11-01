5974be320b3dbea517a839c640e167403c15c3f8
qiwi <qiwi@qiwi.qiwi>
2025-11-01 13:39:40 +0300

    added filling function

--- main.cpp
+++ main.cpp
@@ -8,6 +8,15 @@ void destroy(int **mtx, size_t created) {
   delete[] mtx;
 }
 
+int ** constuction( int ** mtx, int init, size_t rows, size_t columns) {
+    for (size_t i =0;i < rows;++i){
+        for (size_t j =0 ; j < columns; ++j) {
+            mtx[i][j] = init;
+        }
+    }
+    return mtx; 
+}
+
 int **create(size_t rows, size_t columns) {
   int **mtx = new int *[rows];
   size_t progress = 0;
@@ -25,8 +34,10 @@ int main() {
   int **matrix = nullptr;
   try {
     matrix = create(5, 6);
-
+    matrix = constuction(matrix,1,5, 6);
   } catch (const std::bad_alloc &e) {
     std::cout << e.what() << std::endl;
   }
 }
+
+
